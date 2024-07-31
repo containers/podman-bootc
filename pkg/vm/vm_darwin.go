523693508b21cf46f9936bebd51f834e26b64423
German Maglione <gmaglione@redhat.com>
2024-07-31 16:26:17 +0200

    Switch to krunkit on macOS
    Signed-off-by: German Maglione <gmaglione@redhat.com>

--- pkg/vm/vm_darwin.go
+++ pkg/vm/vm_darwin.go
@@ -1,13 +1,11 @@
 package vm
 
 import (
-	"errors"
 	"fmt"
-	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"time"
+	"strconv"
 
 	"github.com/containers/podman-bootc/pkg/config"
 	"github.com/containers/podman-bootc/pkg/utils"
@@ -56,31 +54,7 @@ func (b *BootcVMMac) CloseConnection() {
 }
 
 func (b *BootcVMMac) PrintConsole() (err error) {
-	//qemu seems to asynchronously create the socket file
-	//so this will wait up to a few seconds for socket to be created
-	socketCreationTimeout := 5 * time.Second
-	elapsed := 0 * time.Millisecond
-	interval := 100 * time.Millisecond
-	for elapsed < socketCreationTimeout {
-		time.Sleep(interval) //always sleep a little bit at the start
-		elapsed += interval
-		if _, err = os.Stat(b.socketFile); err == nil {
-			break
-		}
-	}
-
-	c, err := net.Dial("unix", b.socketFile)
-	if err != nil {
-		return fmt.Errorf("error connecting to socket %s", err)
-	}
-	for {
-		buf := make([]byte, 8192)
-		_, err := c.Read(buf)
-		if err != nil {
-			return fmt.Errorf("error reading socket %s", err)
-		}
-		print(string(buf))
-	}
+	return nil
 }
 
 func (b *BootcVMMac) GetConfig() (cfg *BootcVMConfig, err error) {
@@ -118,48 +92,24 @@ func (b *BootcVMMac) Run(params RunVMParameters) (err error) {
 		}
 	}
 
-	var args []string
-	args = append(args, "-display", "none")
-	args = append(args, "-chardev", fmt.Sprintf("socket,id=char0,server=on,wait=off,path=%s", b.socketFile), "-serial", "chardev:char0")
-
-	args = append(args, "-cpu", "host")
-	args = append(args, "-m", "2G")
-	args = append(args, "-smp", "2")
-	args = append(args, "-snapshot")
-	nicCmd := fmt.Sprintf("user,model=virtio-net-pci,hostfwd=tcp::%d-:22", b.sshPort)
-	args = append(args, "-nic", nicCmd)
-
-	vmPidFile := filepath.Join(b.cacheDir, "run.pid")
-	args = append(args, "-pidfile", vmPidFile)
-
-	vmDiskImage := filepath.Join(b.cacheDir, config.DiskImage)
-	driveCmd := fmt.Sprintf("if=virtio,format=raw,file=%s", vmDiskImage)
-	args = append(args, "-drive", driveCmd)
-
-	err = b.ParseCloudInit()
+	execPath, err := os.Executable()
 	if err != nil {
-		return err
+		return fmt.Errorf("getting executable path: %w", err)
 	}
 
-	if b.hasCloudInit {
-		args = append(args, "-cdrom", b.cloudInitArgs)
-	}
-
-	if b.sshIdentity != "" {
-		smbiosCmd, err := b.oemString()
-		if err != nil {
-			return err
-		}
-
-		args = append(args, "-smbios", smbiosCmd)
-	}
-
-	cmd, err := b.createQemuCommand()
+	execPath, err = filepath.EvalSymlinks(execPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("following executable symlink: %w", err)
 	}
 
-	cmd.Args = append(cmd.Args, args...)
+	execPath, err = filepath.Abs(execPath)
+	if err != nil {
+		return fmt.Errorf("getting executable absolute path: %w", err)
+	}
+
+	args := []string{"vmmon", b.imageID, b.vmUsername, b.sshIdentity, strconv.Itoa(b.sshPort)}
+	cmd := exec.Command(execPath, args...)
+
 	logrus.Debugf("Executing: %v", cmd.Args)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -212,37 +162,6 @@ func (b *BootcVMMac) IsRunning() (bool, error) {
 
 func (b *BootcVMMac) Exists() (bool, error) {
 	return utils.FileExists(b.pidFile)
-}
-
-func (b *BootcVMMac) createQemuCommand() (*exec.Cmd, error) {
-	qemuInstallPath, err := getQemuInstallPath()
-	if err != nil {
-		return nil, err
-	}
-
-	path := qemuInstallPath + "/bin/qemu-system-aarch64"
-	args := []string{
-		"-accel", "hvf",
-		"-cpu", "host",
-		"-M", "virt,highmem=on",
-		"-drive", "file=" + qemuInstallPath + "/share/qemu/edk2-aarch64-code.fd" + ",if=pflash,format=raw,readonly=on",
-	}
-	return exec.Command(path, args...), nil
-}
-
-// Search for a qemu binary, let's check if is shipped with podman v4
-// or if it's installed using homebrew.
-// This function will no longer be necessary as soon as we use libvirt on macos.
-func getQemuInstallPath() (string, error) {
-	dirs := []string{"/opt/homebrew", "/opt/podman/qemu"}
-	for _, d := range dirs {
-		qemuBinary := filepath.Join(d, "bin/qemu-system-aarch64")
-		if _, err := os.Stat(qemuBinary); err == nil {
-			return d, nil
-		}
-	}
-
-	return "", errors.New("QEMU binary not found")
 }
 
 func (v *BootcVMMac) Unlock() error {
